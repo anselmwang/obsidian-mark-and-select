5549ca1078b8762dddc70a8ec55fd27c1247c985
Yu Wang <yuwan@microsoft.com>
2022-02-17 21:16:38 -0800

    feat: add icon for command

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ export default class AdvancedSelectionPlugin extends Plugin {
 		this.addCommand({
 			id: 'set-one-end-of-selection',
 			name: 'Set one end of selection',
+			icon: 'pencil',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getCursor());
 				this.one_end_position= editor.getCursor();
@@ -15,6 +16,7 @@ export default class AdvancedSelectionPlugin extends Plugin {
 		this.addCommand({
 			id: 'select',
 			name: 'Select from one end',
+			icon: 'up-and-down-arrows',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				if(this.one_end_position != null)
 				{
